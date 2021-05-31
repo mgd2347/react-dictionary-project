73c8dcf6e2a37a18dc68e815c87d9b7af83ccbfb
mgd2347 <margaridaf235@gmail.com>
2021-05-31 15:23:21 +0100

    Added bootstrap and a footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ function App() {
           Just a dictionary
         </p>
       </header>
+      <footer className="App-footer">
+        <a href="https://github.com/mgd2347/react-dictionary-project" target="_blank" rel="noreferrer">
+          Open-source code
+        </a>
+        { } by { }
+        <a href="https://www.instagram.com/emefialho_fz/" target="_blank" rel="noreferrer">
+          Maggie F.
+        </a>
+      </footer>
     </div>
   );
 }
